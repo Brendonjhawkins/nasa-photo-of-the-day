ee545fac40a101df3406700dd7c4cc907ff1d159
BrendonHawkins <artbitcodedata@gmail.com>
2021-08-12 21:04:16 -0400

    still for behind

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
-import { BASE_URL, API_KEY } from '../constants'
+import {BASE_URL, API_KEY } from '../src/constants/index'
 import axios from 'axios'
 
-
+// import Header from './Header'
+// import Credentials from './Credentials'
+// import Title from './Title'
+// import Date from './Date'
+// import Photo from './Photo'
 
 
 export default function App() {
-  const [photo, setPhoto] = useState()
+  const [data, setData] = useState()
+  // const [photo, setPhoto] = useState
 
 
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res => {
+      console.log(res.data)
+      setData(res.data)
+      console.log(setData)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, [])
+
+  
 
 
 
@@ -26,7 +45,7 @@ export default function App() {
     <div className="App">
       <di>
         <h1>NASA Photo of the Day</h1>
-        <img src= />
+        {/* <img src= /> */}
       </di>
     </div>
   );
